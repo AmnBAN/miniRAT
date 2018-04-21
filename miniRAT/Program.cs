d68ac2b05bfea5e60475966a1c4e8c3ef910183e
Pouya <it.pouya@gmail.com>
2018-04-21 16:30:21 +0430

    demo txts

---
+++ miniRAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace miniRAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Keep calm miniRAT starts");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("miniRAT end");
+            Console.ReadKey();
+        }
+    }
+}
